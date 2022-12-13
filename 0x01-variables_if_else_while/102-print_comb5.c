c91da7e075fa142d6ab1b758a0eb8c5cd00c90ad
chinaza Elui <eluichinaza@gmail.com>
2022-12-13 20:31:19 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,38 +7,40 @@
  */
 int main(void)
 {
-	int c, i, k, j;
-
-	for (c = 48; c <= 57; c++)
+	int c = 0;
+	int f_d;
+	int l_d;
+	
+	int c2;
+	int f_d2;
+	int l_d2;
+	
+	while (c <= 98)
 	{
-		for (i = 48; i <= 57; i++)
+		f_d = (c / 10 + 'o');
+		l_d = (c % 10 + '0');
+		c2 = 0;
+		while (c2 <= 99)
 		{
-			for (k = 48; k <= 57; k++)
+			f_d2 = (c2 / 10 + '0');
+			l_d2 = (c2 % 10 + '0');
+			
+			if (c < c2)
 			{
-				for (j = 48; j <= 57; j++)
+				putchar(f_d);
+				putchar(l_d);
+				putchar(' ');
+				putchar(f_d2);
+				putchar(l_d2);
+				
+				if (c != 98)
 				{
-					if (((k + j) > (c + i) &&  k >= c) || c < k)
-					{
-						putchar(c);
-						putchar(i);
-						putchar(' ');
-						putchar(k);
-						putchar(j);
-
-						if (c + i + k + j == 227 && c == 57)
-						{
-						break;
-						}
-						else
-						{
-						putchar(',');
-						putchar(' ');
-						}
-					}
+					putchar(' , ');
+					putchar(' ');
 				}
 			}
+			c2++;
 		}
+		c++;
+		return (0);
 	}
-	putchar('\n');
-	return (0);
-}
