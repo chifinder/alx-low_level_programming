f59a0696f6ca67cd6adcf491c32a18c74d3bea9f
chinaza Elui <eluichinaza@gmail.com>
2022-12-13 20:45:31 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,11 +10,9 @@ int main(void)
 	int c = 0;
 	int f_d;
 	int l_d;
-	
 	int c2;
 	int f_d2;
 	int l_d2;
-	
 	while (c <= 98)
 	{
 		f_d = (c / 10 + 'o');
@@ -23,8 +21,7 @@ int main(void)
 		while (c2 <= 99)
 		{
 			f_d2 = (c2 / 10 + '0');
-			l_d2 = (c2 % 10 + '0');
-			
+			l_d2 = (c2 % 10 + '0');	
 			if (c < c2)
 			{
 				putchar(f_d);
