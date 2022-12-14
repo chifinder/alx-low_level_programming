2c6c73b35ace606fab34823a667e84ad06f3843d
chifinder <eluichinaza@gmail.com>
2022-12-14 11:59:16 -0800

    abeg

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Desc: header file containing prototypes for all functions
+ * written in the 0x02-functions_nested_loops directory
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+
+#endif
